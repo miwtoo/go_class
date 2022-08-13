f730f288daeb84fee0f11296027468327b5b9ff7
Miwtoo <miwyugioh@gmail.com>
2022-08-13 09:57:08 +0700

    feat: :sparkles: ex squareArea

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
 func main()  {
-	println("Hello, World!")	
+	println("Hello, World!")
+
+	print("squareArea: ", squareArea(2))
+}
+
+func squareArea(a float64) float64  {
+	return a * a
 }
