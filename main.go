fc434a60f2dbc6295a889798361c09986eb7274f
Miwtoo <miwyugioh@gmail.com>
2022-08-13 10:01:44 +0700

    feat: :sparkles: add swap

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ func main()  {
 	println("Hello, World!")
 
 	fmt.Println("squareArea: ", squareArea(2))
+
+	a, b := swap(1, 2)
+	fmt.Println("swap(1, 2): ", a, b)
+}
+
+func swap(a,b int) (int, int) {
+	return b, a
 }
 
 func squareArea(a float64) float64  {
