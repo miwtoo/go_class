b43ce230d7cdbb17f60561fb8413fceb45376ea8
Miwtoo <miwyugioh@gmail.com>
2022-08-13 10:05:35 +0700

    feat: :sparkles: add if

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ func main()  {
 
 	a, b := swap(1, 2)
 	fmt.Println("swap(1, 2): ", a, b)
+
+	if ok := isCorrect(); ok {
+		println("it' correct")
+	}
+}
+
+func isCorrect() bool {
+	return true
 }
 
 func swap(a,b int) (int, int) {
